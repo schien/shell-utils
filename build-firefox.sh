80a71f9f7b28400955e30ba1fb8fc537c56337eb
Shih-Chiang Chien <schien@mozilla.com>
2014-02-10 18:23:55 +0800

    use abspath for MOZCONFIG

--- build-firefox.sh
+++ build-firefox.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-MOZCONFIG=.mozconfig-firefox ./mach build
+COMMAND=${1:-build}
+CONFIG_FILE=$(pwd)/.mozconfig-firefox
+
+shift 1
+PARAMS=$@
+
+MOZCONFIG=$CONFIG_FILE ./mach $COMMAND $PARAMS
