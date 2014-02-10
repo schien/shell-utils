1ea71bc20d18dfdee258b6b85ad4bdbed7f93e48
Shih-Chiang Chien <schien@mozilla.com>
2014-02-10 10:46:40 +0800

    fix build option in b2g-sync.sh

--- b2g-sync.sh
+++ b2g-sync.sh
@@ -2,8 +2,8 @@
 # usage : b2g-sync.sh [build]
 
 if [ "$1" == "build" ]
-git pull upstream master && ./repo sync
-then
 git pull upstream master && ./repo sync && ./build.sh
+then
+git pull upstream master && ./repo sync
 fi
 
